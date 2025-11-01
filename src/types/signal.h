9d2e679ae64be67d1b5a95b83871a3e7ec6ee1e2
Timofey Kirichenko <timofeykirichenko@gmail.com>
2025-11-02 02:19:35 +0300

    Now Signal is safe and with mutexes. Added SignalUnsafe without any mutexes

--- src/types/signal.h
+++ src/types/signal.h
@@ -3,6 +3,7 @@
 #include <types/types.h>
 #include <types/vector.h>
 #include <ex_alloc/chunk_allocator.h>
+#include "platform/mutex.h"
 
 
 /**
@@ -27,12 +28,13 @@ vector_template_def(SignalCallback, SignalCallback);
  * You can connect some functions (callbacks) to the signal. Then in other piece of code, you can emit
  * the signal with your args and all callbacks will be executed.
  *
+ * @warning FOR ONE THREAD USE ONLY. It's not guarded with any mutexes
+ *
  * @api
  */
-typedef struct Signal {
-    // ChunkMemoryAllocator allocator;
+typedef struct SignalUnsafe {
     vec_SignalCallback data;
-} Signal;
+} SignalUnsafe;
 
 /**
  * @api
@@ -40,11 +42,88 @@ typedef struct Signal {
 typedef chunk_allocator_ptr SignalCallbackHandler;
 
 /**
+ * @brief It's like 'signal_unsafe_new_with_params', but for your allocated Signals. Available only in
+ * engine scope;
+ *
+ * @error "InvalidArgument"
+ * @error "AllocationFailed"
+ */
+boolean signal_unsafe_constructor(SignalUnsafe* self);
+
+/**
+ * @brief It's like 'signal_unsafe_free', but for your allocated Signals. Available only in engine scope.
+ *
+ * @error "InvalidArgument"
+ */
+boolean signal_unsafe_destructor(SignalUnsafe* self);
+
+/**
+ * @brief Create a SignalUnsafe instance.
+ *
+ * @error "InvalidArgument"
+ * @error "AllocationFailed"
+ * @api
+ */
+SignalUnsafe* signal_unsafe_new(void);
+
+/**
+ * @brief Free SignalUnsafe instance
+ *
+ * @error "InvalidArgument"
+ * @api
+ */
+boolean signal_unsafe_free(SignalUnsafe* self);
+
+/**
+ * @brief Emit signal. All callbacks will be executed.
+ *
+ * @error "InvalidArgument"
+ * @api
+ */
+boolean signal_unsafe_emit(SignalUnsafe* self, void* args);
+
+/**
+ * @breif Connect callback to the signal. It will be executed, when the signal is emitted.
+ *
+ * @error "InvalidArgument"
+ * @error "AllocationFailed"
+ * @api
+ */
+SignalCallbackHandler signal_unsafe_connect(
+        SignalUnsafe* const self, SignalCallbackFunc func, void* ctx
+);
+
+/**
+ * @brief Disconnect callback from the signal.
+ *
+ * @error "InvalidArgument"
+ * @error "NotFound" if handler out of index boundary or callback by this handler is NULL
+ * @api
+ */
+boolean signal_unsafe_disconnect(SignalUnsafe* self, SignalCallbackHandler handler);
+
+
+/**
+ * @brief It's just a signal with some thread safety (instead of SignalUnsafe)
+ *
+ * I don't know what I should write here, it's intuitively understandable.
+ * You can connect some functions (callbacks) to the signal. Then in other piece of code, you can emit
+ * the signal with your args and all callbacks will be executed.
+ *
+ * @api
+ */
+typedef struct Signal {
+    SignalUnsafe unsafe;
+    mutex_handle mutex;
+} Signal;
+
+/**
  * @brief It's like 'signal_new_with_params', but for your allocated Signals. Available only in engine
  *   scope;
  *
  * @error "InvalidArgument"
  * @error "AllocationFailed"
+ * @error "MutexInitFailed"
  */
 boolean signal_constructor(Signal* self);
 
@@ -87,7 +166,7 @@ boolean signal_emit(Signal* self, void* args);
  * @error "AllocationFailed"
  * @api
  */
-SignalCallbackHandler signal_connect(Signal* self, SignalCallbackFunc func, void* ctx);
+SignalCallbackHandler signal_connect(Signal* const self, SignalCallbackFunc func, void* ctx);
 
 /**
  * @brief Disconnect callback from the signal.
