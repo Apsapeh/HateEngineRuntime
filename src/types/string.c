5d26053c28fb6eb436aa7ef41d10fc4ac55a7675
Timofey Kirichenko <timofeykirichenko@gmail.com>
2025-11-13 02:08:29 +0300

    String API was improved. Some functions need to be implemented and rewrited (new, from, clone)

--- src/types/string.c
+++ src/types/string.c
@@ -7,11 +7,42 @@
 
 static inline boolean string_set_cstr_ex(String*, const char*, const u64);
 static inline boolean string_push_back_cstr_ex(String*, const char*, const u64);
-static inline boolean string_push_front_cstr_ex(String*, const char*, const u64);
 static inline boolean string_utf8_push_back_ex(StringUTF8*, const StringUTF8*);
-static inline boolean string_utf8_push_front_ex(StringUTF8*, const StringUTF8*);
 static inline boolean string_insert_cstr_ex(String*, const char*, const u64, const u64);
 static inline StringUTF8* string_utf8_dec(const char*, const u64);
+
+// TODO: impl
+boolean string_destructor(String* self) {
+    return false;
+}
+
+// TODO: Impl
+boolean string_constructor(String* self) {
+    return false;
+}
+
+// TODO: Impl
+boolean string_from_constructor(String* self, const char* c_str) {
+    return false;
+}
+
+// TODO: Impl
+boolean string_from_slice_constructor(String* self, const StringSlice* slice) {
+    return false;
+}
+
+// TODO: Impl
+boolean string_clone_constructor(String* self, const String* string) {
+    return false;
+}
+
+
+boolean string_free(String* self) {
+    ERROR_ARG_CHECK(self, { return false; });
+    tfree(self->ptr);
+    tfree(self);
+    return true;
+}
 
 String* string_new(void) {
     String* str_new = tmalloc(sizeof(String));
@@ -41,6 +72,25 @@ String* string_from(const char* c_str) {
 
     str->len = c_str_len;
     memcpy(str->ptr, c_str, c_str_len + 1);
+
+    return str;
+}
+
+String* string_from_slice(const StringSlice* str_sl) {
+    ERROR_ARG_CHECK(str_sl, { return NULL; });
+
+    String* str = tmalloc(sizeof(String));
+    ERROR_ALLOC_CHECK(str, { return NULL; });
+
+    str->ptr = tmalloc(str_sl->len + 1);
+    ERROR_ALLOC_CHECK(str->ptr, {
+        tfree(str);
+        return NULL;
+    });
+
+    memcpy(str->ptr, str_sl->str, str_sl->len);
+    str->len = str_sl->len;
+    *(str->ptr + str->len) = '\0';
 
     return str;
 }
@@ -77,14 +127,19 @@ static inline boolean string_set_cstr_ex(String* self, const char* c_str, const 
     return true;
 }
 
+boolean string_set(String* self, const String* str) {
+    ERROR_ARGS_CHECK_2(self, str, { return false; });
+    return string_set_cstr_ex(self, str->ptr, str->len);
+}
+
 boolean string_set_cstr(String* self, const char* c_str) {
     ERROR_ARGS_CHECK_2(self, c_str, { return false; });
     return string_set_cstr_ex(self, c_str, strlen(c_str));
 }
 
-boolean string_set(String* self, const String* str) {
+boolean string_set_slice(String* self, const StringSlice* str) {
     ERROR_ARGS_CHECK_2(self, str, { return false; });
-    return string_set_cstr_ex(self, str->ptr, str->len);
+    return string_set_cstr_ex(self, str->str, str->len);
 }
 
 char* string_cstr(const String* self) {
@@ -97,6 +152,7 @@ u64 string_len(const String* const c_str, boolean* success) {
         if (success) {
             *success = false;
         }
+        return 0;
     });
     //*dest_len = c_str->len;
     if (success) {
@@ -116,6 +172,11 @@ boolean string_push_back_cstr(String* self, const char* src) {
     return string_push_back_cstr_ex(self, src, strlen(src));
 }
 
+boolean string_push_back_slice(String* self, const StringSlice* src) {
+    ERROR_ARGS_CHECK_2(self, src, { return false; });
+    return string_push_back_cstr_ex(self, src->str, src->len);
+}
+
 static inline boolean string_push_back_cstr_ex(String* self, const char* src, const u64 len_src) {
     char* tmp_ptr = trealloc(self->ptr, self->len + len_src + 1);
     ERROR_ALLOC_CHECK(tmp_ptr, { return false; });
@@ -132,52 +193,33 @@ static inline boolean string_push_back_cstr_ex(String* self, const char* src, co
     return true;
 }
 
-boolean string_push_front(String* self, const String* src) {
+
+boolean string_insert(String* self, const String* src, const u64 i) {
     ERROR_ARGS_CHECK_2(self, src, { return false; });
-    return string_push_front_cstr_ex(self, src->ptr, src->len);
-}
-
-boolean string_push_front_cstr(String* self, const char* src) {
-    ERROR_ARGS_CHECK_2(self, src, { return false; });
-    return string_push_front_cstr_ex(self, src, strlen(src));
-}
-
-static inline boolean string_push_front_cstr_ex(String* self, const char* src, const u64 len_src) {
-    char* tmp_ptr = trealloc(self->ptr, self->len + len_src + 1);
-    ERROR_ALLOC_CHECK(tmp_ptr, { return false; });
-
-    if (src >= self->ptr && src <= self->ptr + self->len - 1) {
-        memmove(tmp_ptr + len_src, tmp_ptr, self->len + 1);
-        memcpy(tmp_ptr, tmp_ptr + (src - self->ptr) + len_src, len_src);
-    } else {
-        memmove(tmp_ptr + len_src, tmp_ptr, self->len + 1);
-        memcpy(tmp_ptr, src, len_src);
-    }
-
-    self->ptr = tmp_ptr;
-    self->len += len_src;
-
-    return true;
+    return string_insert_cstr_ex(self, src->ptr, i, src->len);
 }
 
 boolean string_insert_cstr(String* self, const char* src, const u64 i) {
     ERROR_ARGS_CHECK_2(self, src, { return false; });
+    return string_insert_cstr_ex(self, src, i, strlen(src));
+}
 
+boolean string_insert_slice(String* self, const StringSlice* src, const u64 i) {
+    ERROR_ARGS_CHECK_2(self, src, { return false; });
+    return string_insert_cstr_ex(self, src->str, i, src->len);
+}
+
+static inline boolean string_insert_cstr_ex(
+        String* self, const char* src, const u64 i, const u64 len_src
+) {
     if (i > self->len) {
         LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
         set_error(ERROR_INVALID_ARGUMENT);
         return false;
     }
 
-    return string_insert_cstr_ex(self, src, i, strlen(src));
-}
-
-
-static inline boolean string_insert_cstr_ex(
-        String* self, const char* src, const u64 i, const u64 len_src
-) {
-    if (i == 0)
-        return string_push_front_cstr_ex(self, src, len_src);
+    // if (i == 0)
+    //     return string_push_front_cstr_ex(self, src, len_src);
 
     if (i == self->len)
         return string_push_back_cstr_ex(self, src, len_src);
@@ -203,17 +245,6 @@ static inline boolean string_insert_cstr_ex(
     return true;
 }
 
-boolean string_insert(String* self, const String* src, const u64 i) {
-    ERROR_ARGS_CHECK_2(self, src, { return false; });
-
-    if (i > self->len) {
-        LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
-        set_error(ERROR_INVALID_ARGUMENT);
-        return false;
-    }
-
-    return string_insert_cstr_ex(self, src->ptr, i, src->len);
-}
 
 boolean string_remove(String* self, const u64 i) {
     ERROR_ARG_CHECK(self, { return false; });
@@ -309,12 +340,6 @@ boolean string_equals_slice(const String* str, StringSlice* slice) {
     return memcmp(str->ptr, slice->str, str->len) ? false : true;
 }
 
-boolean string_free(String* self) {
-    ERROR_ARG_CHECK(self, { return false; });
-    tfree(self->ptr);
-    tfree(self);
-    return true;
-}
 
 boolean string_get_slice_constructor(
         StringSlice* self, const String* str, const u64 from, const u64 to
@@ -373,24 +398,6 @@ StringSlice* string_slice_from_cstr(const char* c_str, const i64 size) {
     }
 }
 
-String* string_from_slice(const StringSlice* str_sl) {
-    ERROR_ARG_CHECK(str_sl, { return NULL; });
-
-    String* str = tmalloc(sizeof(String));
-    ERROR_ALLOC_CHECK(str, { return NULL; });
-
-    str->ptr = tmalloc(str_sl->len + 1);
-    ERROR_ALLOC_CHECK(str->ptr, {
-        tfree(str);
-        return NULL;
-    });
-
-    memcpy(str->ptr, str_sl->str, str_sl->len);
-    str->len = str_sl->len;
-    *(str->ptr + str->len) = '\0';
-
-    return str;
-}
 
 boolean string_slice_equals(StringSlice* str_sl_1, StringSlice* str_sl_2) {
     ERROR_ARGS_CHECK_2(str_sl_1, str_sl_2, { return false; });
@@ -398,28 +405,6 @@ boolean string_slice_equals(StringSlice* str_sl_1, StringSlice* str_sl_2) {
         return false;
 
     return memcmp(str_sl_1->str, str_sl_2->str, str_sl_1->len) ? false : true;
-}
-
-boolean string_push_back_slice(String* self, const StringSlice* src) {
-    ERROR_ARGS_CHECK_2(self, src, { return false; });
-    return string_push_back_cstr_ex(self, src->str, src->len);
-}
-
-boolean string_push_front_slice(String* self, const StringSlice* src) {
-    ERROR_ARGS_CHECK_2(self, src, { return false; });
-    return string_push_front_cstr_ex(self, src->str, src->len);
-}
-
-boolean string_insert_slice(String* self, const StringSlice* src, const u64 i) {
-    ERROR_ARGS_CHECK_2(self, src, { return false; });
-
-    if (i > self->len) {
-        LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
-        set_error(ERROR_INVALID_ARGUMENT);
-        return false;
-    }
-
-    return string_insert_cstr_ex(self, src->str, i, src->len);
 }
 
 boolean string_slice_free(StringSlice* self) {
@@ -430,11 +415,51 @@ boolean string_slice_free(StringSlice* self) {
 
 //<--------------------------- UTF-8 --------------------------->
 
+// TODO: Impl
+boolean string_utf8_destructor(StringUTF8* self) {
+    return false;
+}
+
+// TODO: impl
+boolean string_utf8_constructor(StringUTF8* self) {
+    return false;
+}
+
+// TODO: impl
+boolean string_utf8_from_constructor(StringUTF8* self, const char* c_str) {
+    return false;
+}
+
+// TODO: impl
+boolean string_utf8_from_string_constructor(StringUTF8* self, const String* string) {
+    return false;
+}
+
+// TODO: impl
+boolean string_utf8_from_slice_constructor(StringUTF8* self, const StringSlice* slice) {
+    return false;
+}
+
+// TODO: impl
+boolean string_utf8_clone_constructor(StringUTF8* self, const StringUTF8* other_string) {
+    return false;
+}
+
+
+boolean string_utf8_free(StringUTF8* self) {
+    ERROR_ARG_CHECK(self, { return false; });
+    tfree(self->ptr);
+    tfree(self);
+
+    return true;
+}
+
 StringUTF8* string_utf8_new(void) {
     StringUTF8* str_new = tmalloc(sizeof(StringUTF8));
     ERROR_ALLOC_CHECK(str_new, { return NULL; });
 
     str_new->len = 0;
+    str_new->ptr = NULL;
 
     return str_new;
 }
@@ -445,7 +470,7 @@ static inline StringUTF8* string_utf8_dec(const char* c_str, const u64 len_c_str
         return string_utf8_new();
     }
 
-    u32* cache = tmalloc(4 * len_c_str);
+    u32* cache = tmalloc(sizeof(u32) * len_c_str);
     ERROR_ALLOC_CHECK(cache, { return NULL; });
 
     StringUTF8* str_new = tmalloc(sizeof(StringUTF8));
@@ -479,7 +504,7 @@ static inline StringUTF8* string_utf8_dec(const char* c_str, const u64 len_c_str
         }
     }
 
-    u64 size_new_str = 4 * (tmp_ptr - cache); // размер получившейся строки в байтах
+    usize size_new_str = sizeof(u32) * (tmp_ptr - cache); // размер получившейся строки в байтах
     str_new->ptr = trealloc(cache, size_new_str);
     ERROR_ALLOC_CHECK(str_new->ptr, {
         tfree(cache);
@@ -495,6 +520,35 @@ static inline StringUTF8* string_utf8_dec(const char* c_str, const u64 len_c_str
 StringUTF8* string_utf8_from(const char* c_str) {
     ERROR_ARG_CHECK(c_str, { return NULL; });
     return string_utf8_dec(c_str, strlen(c_str));
+}
+
+StringUTF8* string_utf8_from_string(const String* string) {
+    ERROR_ARG_CHECK(string, { return NULL; });
+    return string_utf8_dec(string->ptr, string->len);
+}
+
+StringUTF8* string_utf8_from_slice(const StringSlice* slice) {
+    ERROR_ARG_CHECK(slice, { return NULL; });
+    return string_utf8_dec(slice->str, slice->len);
+}
+
+StringUTF8* string_utf8_clone(const StringUTF8* str) {
+    ERROR_ARG_CHECK(str, { return NULL; });
+
+    StringUTF8* str_new = tmalloc(sizeof(String));
+    ERROR_ALLOC_CHECK(str_new, { return NULL; });
+
+    u64 size_str = sizeof(u32) * str->len;
+    str_new->ptr = tmalloc(size_str);
+    ERROR_ALLOC_CHECK(str_new->ptr, {
+        tfree(str_new);
+        return NULL;
+    });
+
+    str_new->len = str->len;
+    memcpy(str_new->ptr, str->ptr, size_str);
+
+    return str_new;
 }
 
 String* string_utf8_to_string(const StringUTF8* self) {
@@ -576,50 +630,32 @@ String* string_utf8_to_string(const StringUTF8* self) {
 }
 
 
-boolean string_utf8_len(const StringUTF8* self, u64* const dest_len) {
-    ERROR_ARGS_CHECK_2(self, dest_len, { return false; });
-    *dest_len = self->len;
-    return true;
-}
-
-boolean string_utf8_size(const StringUTF8* self, u64* const dest_len) {
-    ERROR_ARGS_CHECK_2(self, dest_len, { return false; });
-    *dest_len = 4 * self->len;
-    return true;
-}
-
-
-StringUTF8* string_utf8_clone(const StringUTF8* str) {
-    ERROR_ARG_CHECK(str, { return NULL; });
-
-    StringUTF8* str_new = tmalloc(sizeof(String));
-    ERROR_ALLOC_CHECK(str_new, { return NULL; });
-
-    u64 size_str = 4 * str->len;
-    str_new->ptr = tmalloc(size_str);
-    ERROR_ALLOC_CHECK(str_new->ptr, {
-        tfree(str_new);
-        return NULL;
+u64 string_utf8_len(const StringUTF8* self, boolean* success) {
+    ERROR_ARGS_CHECK_1(self, {
+        if (success)
+            *success = false;
+        return 0;
     });
 
-    str_new->len = str->len;
-    memcpy(str_new->ptr, str->ptr, size_str);
+    if (success)
+        *success = true;
 
-    return str_new;
+    return self->len;
 }
 
+u64 string_utf8_size(const StringUTF8* self, boolean* success) {
+    ERROR_ARGS_CHECK_1(self, {
+        if (success)
+            *success = false;
+        return 0;
+    });
 
-boolean string_utf8_push_back_cstr(StringUTF8* self, char* src) {
-    ERROR_ARGS_CHECK_2(self, src, { return false; });
+    if (success)
+        *success = true;
 
-    StringUTF8* str_new = string_utf8_dec(src, strlen(src));
-    ERROR_ALLOC_CHECK(str_new, { return false; });
-
-    boolean status = string_utf8_push_back_ex(self, str_new);
-    string_utf8_free(str_new);
-
-    return status;
+    return sizeof(u32) * self->len;
 }
+
 
 static inline boolean string_utf8_push_back_ex(StringUTF8* self, const StringUTF8* src) {
     const u64 size_dest = 4 * self->len;
@@ -644,9 +680,57 @@ boolean string_utf8_push_back(StringUTF8* self, const StringUTF8* src) {
     return string_utf8_push_back_ex(self, src);
 }
 
+boolean string_utf8_push_back_cstr(StringUTF8* self, const char* src) {
+    ERROR_ARGS_CHECK_2(self, src, { return false; });
+
+    StringUTF8 str_new;
+    boolean status = string_utf8_from_constructor(&str_new, src);
+    if (status == false)
+        return false;
+
+    status = string_utf8_push_back_ex(self, &str_new);
+    string_utf8_destructor(&str_new);
+
+    return status;
+}
+
+boolean string_utf8_push_back_string(StringUTF8* self, const String* src) {
+    ERROR_ARGS_CHECK_2(self, src, { return false; });
+
+    StringUTF8 str_new;
+    boolean status = string_utf8_from_string_constructor(&str_new, src);
+    if (status == false)
+        return false;
+
+    status = string_utf8_push_back_ex(self, &str_new);
+    string_utf8_destructor(&str_new);
+
+    return status;
+}
+
+boolean string_utf8_push_back_slice(StringUTF8* self, const StringSlice* src) {
+    ERROR_ARGS_CHECK_2(self, src, { return false; });
+
+    StringUTF8 str_new;
+    boolean status = string_utf8_from_slice_constructor(&str_new, src);
+    if (status == false)
+        return false;
+
+    status = string_utf8_push_back_ex(self, &str_new);
+    string_utf8_destructor(&str_new);
+
+    return status;
+}
+
 static inline boolean string_utf8_insert_ex(StringUTF8* self, const StringUTF8* src, const usize i) {
-    if (i == 0)
-        return string_utf8_push_front_ex(self, src);
+    if (i > self->len) {
+        LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
+        set_error(ERROR_INVALID_ARGUMENT);
+        return false;
+    }
+
+    // if (i == 0)
+    //     return string_utf8_push_front_ex(self, src);
 
     if (i == self->len)
         return string_utf8_push_back_ex(self, src);
@@ -675,53 +759,9 @@ static inline boolean string_utf8_insert_ex(StringUTF8* self, const StringUTF8* 
     return true;
 }
 
-boolean string_utf8_push_front(StringUTF8* self, const StringUTF8* src) {
+
+boolean string_utf8_insert(StringUTF8* self, const StringUTF8* src, const u64 i) {
     ERROR_ARGS_CHECK_2(self, src, { return false; });
-    return string_utf8_push_front_ex(self, src);
-}
-
-static inline boolean string_utf8_push_front_ex(StringUTF8* self, const StringUTF8* src) {
-    u64 size_dest = 4 * self->len;
-    u64 size_src = 4 * src->len;
-
-    u32* tmp_ptr = trealloc(self->ptr, size_dest + size_src);
-    ERROR_ALLOC_CHECK(tmp_ptr, { return false; });
-
-    if (src->ptr >= self->ptr && src->ptr < self->ptr + self->len) {
-        memmove(tmp_ptr + src->len, tmp_ptr, size_dest);
-        memcpy(tmp_ptr, tmp_ptr + (src->ptr - self->ptr), size_src);
-    } else {
-        memmove(tmp_ptr + src->len, tmp_ptr, size_dest);
-        memcpy(tmp_ptr, src->ptr, size_src);
-    }
-
-    self->ptr = tmp_ptr;
-    self->len += src->len;
-
-    return true;
-}
-
-
-boolean string_utf8_push_front_cstr(StringUTF8* self, char* src) {
-    ERROR_ARGS_CHECK_2(self, src, { return false; });
-
-    StringUTF8* str_new = string_utf8_dec(src, strlen(src));
-    ERROR_ALLOC_CHECK(str_new, { return false; });
-
-    boolean status = string_utf8_push_front_ex(self, str_new);
-    string_utf8_free(str_new);
-
-    return status;
-}
-
-boolean string_utf8_insert(StringUTF8* self, const StringUTF8* src, const usize i) {
-    ERROR_ARGS_CHECK_2(self, src, { return false; });
-
-    if (i > self->len) {
-        LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
-        set_error(ERROR_INVALID_ARGUMENT);
-        return false;
-    }
 
     return string_utf8_insert_ex(self, src, i);
 }
@@ -730,17 +770,41 @@ boolean string_utf8_insert(StringUTF8* self, const StringUTF8* src, const usize 
 boolean string_utf8_insert_cstr(StringUTF8* self, const char* src, const u64 i) {
     ERROR_ARGS_CHECK_2(self, src, { return false; });
 
-    if (i > self->len) {
-        LOG_ERROR_OR_DEBUG_FATAL("Invalid argument (output for boundary): i");
-        set_error(ERROR_INVALID_ARGUMENT);
+    StringUTF8 str_new;
+    boolean status = string_utf8_from_constructor(&str_new, src);
+    if (status == false)
         return false;
-    }
 
-    StringUTF8* str_new = string_utf8_dec(src, strlen(src));
-    ERROR_ALLOC_CHECK(str_new, { return false; });
+    status = string_utf8_insert_ex(self, &str_new, i);
+    string_utf8_destructor(&str_new);
 
-    boolean status = string_utf8_insert_ex(self, str_new, i);
-    string_utf8_free(str_new);
+    return status;
+}
+
+boolean string_utf8_insert_string(StringUTF8* self, const String* src, const u64 i) {
+    ERROR_ARGS_CHECK_2(self, src, { return false; });
+
+    StringUTF8 str_new;
+    boolean status = string_utf8_from_string_constructor(&str_new, src);
+    if (status == false)
+        return false;
+
+    status = string_utf8_insert_ex(self, &str_new, i);
+    string_utf8_destructor(&str_new);
+
+    return status;
+}
+
+boolean string_utf8_insert_slice(StringUTF8* self, const StringSlice* src, const u64 i) {
+    ERROR_ARGS_CHECK_2(self, src, { return false; });
+
+    StringUTF8 str_new;
+    boolean status = string_utf8_from_slice_constructor(&str_new, src);
+    if (status == false)
+        return false;
+
+    status = string_utf8_insert_ex(self, &str_new, i);
+    string_utf8_destructor(&str_new);
 
     return status;
 }
@@ -824,10 +888,6 @@ boolean string_utf8_remove_n(StringUTF8* self, const u64 i, const u64 n) {
     return true;
 }
 
-StringUTF8* string_to_string_utf8(const String* str) {
-    ERROR_ARG_CHECK(str, { return NULL; });
-    return string_utf8_dec(str->ptr, str->len);
-}
 
 boolean string_utf8_equals(const StringUTF8* str1, const StringUTF8* str2) {
     ERROR_ARGS_CHECK_2(str1, str2, { return false; });
@@ -837,12 +897,3 @@ boolean string_utf8_equals(const StringUTF8* str1, const StringUTF8* str2) {
 
     return memcmp(str1->ptr, str2->ptr, 4 * str1->len) ? false : true;
 }
-
-boolean string_utf8_free(StringUTF8* self) {
-    ERROR_ARG_CHECK(self, { return false; });
-    tfree(self->ptr);
-    tfree(self);
-
-    return true;
-}
-
