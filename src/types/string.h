e29f7541241763d7a98bfbbdc556d376c38fddf2
Timofey Kirichenko <timofeykirichenko@gmail.com>
2025-11-13 02:08:29 +0300

    String API was improved. Some functions need to be implemented and rewrited (new, from, clone)

--- src/types/string.h
+++ src/types/string.h
@@ -24,6 +24,28 @@ typedef struct {
 // src/types/signal.h/signal_constructor and src/types/singal.h/signal_destructor
 // THEY ARE NON-API
 
+// TODO: doc
+boolean string_destructor(String* self);
+
+// TODO: doc
+boolean string_constructor(String* self);
+
+// TODO: doc
+boolean string_from_constructor(String* self, const char* c_str);
+
+// TODO: doc
+boolean string_from_slice_constructor(String* self, const StringSlice* slice);
+
+// TODO: doc
+boolean string_clone_constructor(String* self, const String* string);
+
+/**
+ * @brief free memory of passed String
+ *
+ * @error ERROR_INVALID_ARGUMENT
+ * @api
+ */
+boolean string_free(String* self);
 
 /**
  * @brief create empty String with null-terminator
@@ -43,6 +65,15 @@ String* string_new(void);
 String* string_from(const char* c_str);
 
 /**
+ * @brief String from StringSlice
+ *
+ * @error ERROR_INVALID_ARGUMENT
+ * @error ERROR_ALLOCATION_FAILED
+ * @api
+ */
+String* string_from_slice(const StringSlice* str_sl);
+
+/**
  * @brief clone String by existing String
  *
  * @error ERROR_INVALID_ARGUMENT
@@ -50,6 +81,16 @@ String* string_from(const char* c_str);
  * @api
  */
 String* string_clone(const String* c_str);
+
+/**
+ * @brief update String by existing String
+ * @param str  copied String
+ *
+ * @error ERROR_INVALID_ARGUMENT
+ * @error ERROR_ALLOCATION_FAILED
+ * @api
+ */
+boolean string_set(String* self, const String* str);
 
 /**
  * @brief update String by char*
@@ -62,14 +103,15 @@ String* string_clone(const String* c_str);
 boolean string_set_cstr(String* self, const char* c_str);
 
 /**
- * @brief update String by existing String
+ * @brief update String by StringSlice
  * @param str  copied String
  *
  * @error ERROR_INVALID_ARGUMENT
  * @error ERROR_ALLOCATION_FAILED
  * @api
  */
-boolean string_set(String* self, const String* str);
+boolean string_set_slice(String* self, const StringSlice* str);
+
 
 /**
  * @brief get String by way char*
@@ -109,25 +151,26 @@ boolean string_push_back(String* self, const String* src);
 boolean string_push_back_cstr(String* self, const char* src);
 
 /**
- * @brief add  src-String in the front of self-String
- * @param src  String that need to add
+ * @brief add src-StringSlice to the end of self-String
+ * @param src  StringSlice that need to add
  *
  * @error ERROR_INVALID_ARGUMENT
  * @error ERROR_ALLOCATION_FAILED
  * @api
  */
-boolean string_push_front(String* self, const String* src);
+boolean string_push_back_slice(String* self, const StringSlice* src);
+
 
 /**
- * @brief add  char* in the front of self-String
- * @param src  char* that need to add
+ * @brief insert src-String in self-String
+ * @param src String that need to insert
+ * @param i symbol index
  *
  * @error ERROR_INVALID_ARGUMENT
  * @error ERROR_ALLOCATION_FAILED
  * @api
  */
-boolean string_push_front_cstr(String* self, const char* src);
-
+boolean string_insert(String* self, const String* src, const u64 i);
 
 /**
  * @brief insert char* in self-String
@@ -141,15 +184,16 @@ boolean string_push_front_cstr(String* self, const char* src);
 boolean string_insert_cstr(String* self, const char* src, const u64 i);
 
 /**
- * @brief insert src-String in self-String
- * @param src String that need to insert
+ * @brief insert src-StringSlice to self-String
+ * @param src StringSlice that need to insert
  * @param i symbol index
  *
  * @error ERROR_INVALID_ARGUMENT
  * @error ERROR_ALLOCATION_FAILED
  * @api
  */
-boolean string_insert(String* self, const String* src, const u64 i);
+boolean string_insert_slice(String* self, const StringSlice* src, const u64 i);
+
 
 /**
  * @brief remove symbol under symbol index
@@ -203,14 +247,6 @@ boolean string_equals_cstr(const String* str, const char* c_str);
  */
 boolean string_equals_slice(const String* str, StringSlice* slice);
 
-/**
- * @brief free memory of passed String
- *
- * @error ERROR_INVALID_ARGUMENT
- * @api
- */
-boolean string_free(String* self);
-
 
 /**
  * @brief get StringSlice from String. Use for your allocated data
@@ -251,14 +287,6 @@ StringSlice* string_get_slice(const String* str, const u64 from, const u64 to);
  */
 StringSlice* string_slice_from_cstr(const char* c_str, const i64 size);
 
-/**
- * @brief String from StringSlice
- *
- * @error ERROR_INVALID_ARGUMENT
- * @error ERROR_ALLOCATION_FAILED
- * @api
- */
-String* string_from_slice(const StringSlice* str_sl);
 
 /**
  * @brief check an equality between two StringSlice
@@ -269,37 +297,6 @@ String* string_from_slice(const StringSlice* str_sl);
  * @api
  */
 boolean string_slice_equals(StringSlice* str_sl_1, StringSlice* str_sl_2);
-
-/**
- * @brief add StringSlice in the end of the self-String
- * @param src StringSlice that will be added
- *
- * @error ERROR_INVALID_ARGUMENT
- * @error ERROR_ALLOCATION_FAILED
- * @api
- */
-boolean string_push_back_slice(String* self, const StringSlice* src);
-
-/**
- * @brief add StringSlice in front of self-String
- * @param src StringSlice that will be added
- *
- * @error ERROR_INVALID_ARGUMENT
- * @error ERROR_ALLOCATION_FAILED
- * @api
- */
-boolean string_push_front_slice(String* self, const StringSlice* src);
-
-/**
- * @brief insert StringSlice in String under symbol index i
- * @param src StringSlice that will be inserted
- * @param i   symbol index
- *
- * @error ERROR_INVALID_ARGUMENT
- * @error ERROR_ALLOCATION_FAILED
- * @api
- */
-boolean string_insert_slice(String* self, const StringSlice* src, const u64 i);
 
 /**
  * @brief free memory of passed StringSLice
@@ -321,6 +318,24 @@ typedef struct {
 
 // TODO: Add _constructor and _destructor methodos. Use for reference
 // src/types/signal.h/signal_constructor and src/types/singal.h/signal_destructor
+//
+
+boolean string_utf8_destructor(StringUTF8* self);
+
+// TODO: Add doc
+boolean string_utf8_constructor(StringUTF8* self);
+
+// TODO: doc
+boolean string_utf8_from_constructor(StringUTF8* self, const char* c_str);
+
+// TODO: doc
+boolean string_utf8_from_string_constructor(StringUTF8* self, const String* string);
+
+// TODO: doc
+boolean string_utf8_from_slice_constructor(StringUTF8* self, const StringSlice* slice);
+
+// TODO: doc
+boolean string_utf8_clone_constructor(StringUTF8* self, const StringUTF8* other_string);
 
 /**
  * @brief create empty StringUTF8 with null-terminator
@@ -331,7 +346,7 @@ typedef struct {
 StringUTF8* string_utf8_new(void);
 
 /**
- * @brief create StringUTF8 by char*
+ * @brief create StringUTF8 from const char*. Should be null-treminated
  * @param c_str input data
  *
  * @error ERROR_INVALID_ARGUMENT
@@ -339,6 +354,35 @@ StringUTF8* string_utf8_new(void);
  * @api
  */
 StringUTF8* string_utf8_from(const char* c_str);
+
+/**
+ * @brief create StringUTF8 from String
+ * @param string input data
+ *
+ * @error ERROR_INVALID_ARGUMENT
+ * @error ERROR_ALLOCATION_FAILED
+ * @api
+ */
+StringUTF8* string_utf8_from_string(const String* string);
+
+/**
+ * @brief create StringUTF8 from StringSlice
+ * @param string input data
+ *
+ * @error ERROR_INVALID_ARGUMENT
+ * @error ERROR_ALLOCATION_FAILED
+ * @api
+ */
+StringUTF8* string_utf8_from_slice(const StringSlice* slice);
+
+
+/**
+ * @api
+ * @brief get copy StringUTF8
+ * @error ERROR_INVALID_ARGUMENT
+ * @error ERROR_ALLOCATION_FAILED
+ */
+StringUTF8* string_utf8_clone(const StringUTF8* str);
 
 /**
  * @brief convert StringUTF8 to String
@@ -350,37 +394,29 @@ StringUTF8* string_utf8_from(const char* c_str);
 String* string_utf8_to_string(const StringUTF8* self);
 
 /**
+ * @brief convert StringUTF8 to String pointer. String should be initialized
+ * @param[out] out initialized externally string
+ *
+ * @error ERROR_INVALID_ARGUMENT
+ * @error ERROR_ALLOCATION_FAILED
+ * @api
+ */
+// boolean string_utf8_to_string_ptr(const StringUTF8* self, String* out);
+
+
+/**
  * @api
  * @brief get len of passed StringUTF8
  * @error ERROR_INVALID_ARGUMENT
  */
-boolean string_utf8_len(const StringUTF8* self, u64* const dest_len);
+u64 string_utf8_len(const StringUTF8* self, boolean* success);
 
 /**
  * @api
  * @brief get size of passed StringUTF8
  * @error ERROR_INVALID_ARGUMENT
  */
-boolean string_utf8_size(const StringUTF8* self, u64* const dest_len);
-
-
-/**
- * @api
- * @brief get copy StringUTF8
- * @error ERROR_INVALID_ARGUMENT
- * @error ERROR_ALLOCATION_FAILED
- */
-StringUTF8* string_utf8_clone(const StringUTF8* str);
-
-
-/**
- * @api
- * @brief add char* in last of  StringUTF8
- * @param src input data by way char*
- * @error ERROR_INVALID_ARGUMENT
- * @error ERROR_ALLOCATION_FAILED
- */
-boolean string_utf8_push_back_cstr(StringUTF8* self, char* src);
+u64 string_utf8_size(const StringUTF8* self, boolean* success);
 
 /**
  * @api
@@ -391,6 +427,35 @@ boolean string_utf8_push_back_cstr(StringUTF8* self, char* src);
  */
 boolean string_utf8_push_back(StringUTF8* self, const StringUTF8* src);
 
+/**
+ * @brief add char* in last of  StringUTF8
+ * @param src input data by way char*
+ *
+ * @error ERROR_INVALID_ARGUMENT
+ * @error ERROR_ALLOCATION_FAILED
+ * @api
+ */
+boolean string_utf8_push_back_cstr(StringUTF8* self, const char* src);
+
+/**
+ * @brief add String to the end of  StringUTF8
+ * @param src input data by way char*
+ *
+ * @error ERROR_INVALID_ARGUMENT
+ * @error ERROR_ALLOCATION_FAILED
+ * @api
+ */
+boolean string_utf8_push_back_string(StringUTF8* self, const String* src);
+
+/**
+ * @brief add StringSlice to the end of  StringUTF8
+ * @param src input data by way char*
+ *
+ * @error ERROR_INVALID_ARGUMENT
+ * @error ERROR_ALLOCATION_FAILED
+ * @api
+ */
+boolean string_utf8_push_back_slice(StringUTF8* self, const StringSlice* src);
 
 /**
  * @api
@@ -400,35 +465,40 @@ boolean string_utf8_push_back(StringUTF8* self, const StringUTF8* src);
  * @error ERROR_INVALID_ARGUMENT
  * @error ERROR_ALLOCATION_FAILED
  */
-boolean string_utf8_insert(StringUTF8* self, const StringUTF8* src, const usize i);
+boolean string_utf8_insert(StringUTF8* self, const StringUTF8* src, const u64 i);
 
 /**
- * @api
- * @brief add StringUTF8 in front of StringUTF8
- * @param src input data by way StringUTF8
- * @error ERROR_INVALID_ARGUMENT
- * @error ERROR_ALLOCATION_FAILED
- */
-boolean string_utf8_push_front(StringUTF8* self, const StringUTF8* src);
-
-/**
- * @api
- * @brief add char* in front of  StringUTF8
- * @param src input data by way char*
- * @error ERROR_INVALID_ARGUMENT
- * @error ERROR_ALLOCATION_FAILED
- */
-boolean string_utf8_push_front_cstr(StringUTF8* self, char* src);
-
-/**
- * @api
  * @brief add 'src' by way char* in 'self' under symbol index
  * @param src input data by way char*
  * @param i   symbol index
+ *
  * @error ERROR_INVALID_ARGUMENT
  * @error ERROR_ALLOCATION_FAILED
+ * @api
  */
 boolean string_utf8_insert_cstr(StringUTF8* self, const char* src, const u64 i);
+
+/**
+ * @brief add 'src' by way String in 'self' under symbol index
+ * @param src input data by way char*
+ * @param i   symbol index
+ *
+ * @error ERROR_INVALID_ARGUMENT
+ * @error ERROR_ALLOCATION_FAILED
+ * @api
+ */
+boolean string_utf8_insert_string(StringUTF8* self, const String* src, const u64 i);
+
+/**
+ * @brief add 'src' by way StringSlice in 'self' under symbol index
+ * @param src input data by way char*
+ * @param i   symbol index
+ *
+ * @error ERROR_INVALID_ARGUMENT
+ * @error ERROR_ALLOCATION_FAILED
+ * @api
+ */
+boolean string_utf8_insert_slice(StringUTF8* self, const StringSlice* src, const u64 i);
 
 /**
  * @api
@@ -448,14 +518,6 @@ boolean string_utf8_remove(StringUTF8* self, const u64 i);
  * @error ERROR_ALLOCATION_FAILED
  */
 boolean string_utf8_remove_n(StringUTF8* self, const u64 i, const u64 n);
-
-/**
- * @api
- * @brief converting String to StrinfUTF8
- * @error ERROR_INVALID_ARGUMENT
- * @error ERROR_ALLOCATION_FAILED
- */
-StringUTF8* string_to_string_utf8(const String* str);
 
 /**
  * @api
